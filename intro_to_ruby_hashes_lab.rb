de48fd05f6a63747e72d8518e0ed0dd855f66476
wei chen <weichen9083@gmail.com>
2019-09-19 17:57:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,6 +28,7 @@ def monopoly_with_third_tier
 	                          :names => {},
 	                          :rent_in_dollars =>{
 	                            :one_piece_owned => 25 , 
+	                            :two_pieces_owned => 50,
 	                            
 	                          }
 	                         }
