5a0375fe3efe2355c28ab31f9109a3189a08fa98
wei chen <weichen9083@gmail.com>
2019-09-19 18:01:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,8 +29,6 @@ def monopoly_with_third_tier
 	                          :pennsylvania_railroad => {}, 
 	                          :b_and_o_railroad =>{}, 
 	                          :shortline =>{}
-	                          
-	                         
 	                          },
 	                          :rent_in_dollars =>{
 	                            :one_piece_owned => 25 , 
