f7d2e0e15e75be0309ee870cc100eeda907619cd
wei chen <weichen9083@gmail.com>
2019-09-19 18:01:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
 		monopoly = {:railroads =>{:pieces => 4, 
 	                          :names => {:reading_railroad =>{}, 
 	                          :pennsylvania_railroad => {}, 
-	                          
+	                          :b_and_o_railroad =>{}, 
 	                          
 	                         
 	                          },
