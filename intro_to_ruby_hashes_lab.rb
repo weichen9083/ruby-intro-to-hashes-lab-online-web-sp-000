13d7a1060969d8c428eb556c0451f7ad334fd926
wei chen <weichen9083@gmail.com>
2019-09-19 18:07:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,7 +53,7 @@ def monopoly_with_fourth_tier
 	                             "mortgage_value" => "$400"
 	                          }, 
 	                          :shortline =>{
-	                             "mortgage_value" => '$800'
+	                             "mortgage_value" => "$800"
 	                          }
 	                          },
 	                          :rent_in_dollars =>{
