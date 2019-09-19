3d7b978d54391152e7d85d6d3dfac6837c9e14f6
wei chen <weichen9083@gmail.com>
2019-09-19 18:07:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,8 +49,12 @@ def monopoly_with_fourth_tier
 	                          :pennsylvania_railroad => {
 	                            "mortgage_value" => "$200"
 	                          }, 
-	                          :b_and_o_railroad =>{}, 
-	                          :shortline =>{}
+	                          :b_and_o_railroad =>{
+	                             "mortgage_value" => "$400"
+	                          }, 
+	                          :shortline =>{
+	                             "mortgage_value" => "$800"
+	                          }
 	                          },
 	                          :rent_in_dollars =>{
 	                            :one_piece_owned => 25 , 
